d0ab5105fadafbd5c682c48e94d90454da300612
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-05-14 14:21:19 +0900

    Fix unnecesary import

--- app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/map/MainActivity.java
@@ -13,8 +13,6 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
